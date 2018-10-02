eb50691a41ff2ca54d844465abab5dfca41beb7b
Martin Rist <mlr-github@martinrist.com>
2018-10-02 11:59:16 +0100

    Remove vimproc, ghc-mod and friends + config

--- .vim/bundle/getBundles.sh
+++ .vim/bundle/getBundles.sh
@@ -13,15 +13,8 @@ git clone https://github.com/tpope/vim-commentary.git
 git clone https://github.com/majutsushi/tagbar.git
 
 # Haskell
-git clone https://github.com/eagletmt/neco-ghc
-git clone https://github.com/eagletmt/ghcmod-vim.git
-git clone https://github.com/Shougo/neocomplete.vim.git
 git clone https://github.com/ervandew/supertab.git
 git clone https://github.com/bitc/vim-hdevtools.git
 git clone https://github.com/alx741/vim-hindent.git
 git clone https://github.com/nbouscal/vim-stylish-haskell.git
 git clone https://github.com/Twinside/vim-hoogle.git
-git clone https://github.com/Shougo/vimproc.vim.git
-pushd vimproc.vim
-make
-popd
