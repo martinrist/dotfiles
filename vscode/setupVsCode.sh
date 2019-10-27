c1e7f3e834df3776507718918d70b438bd4a1dad
Martin Rist <mlr-github@martinrist.com>
2019-10-27 11:36:20 +0000

    Correct error message

--- vscode/setupVsCode.sh
+++ vscode/setupVsCode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! [ -x "$(command -v code)" ]; then
-  echo 'Error: `code` command line is not installed.  Please install it using ' >&2
+  echo "Error: 'code' command line is not installed.  Please install it using VSCode's 'Install code command in PATH' command." >&2
   exit 1
 fi
 
